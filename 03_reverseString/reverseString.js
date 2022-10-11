0d672e8b7120af7a022a4884cf13b91fa9438db3
Tristan <tharrisdev@gmail.com>
2022-10-11 18:54:40 -0500

    Add Body To Function To Reverse String Function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split('');
+    splitString.reverse();
+    splitString = splitString.join('');
+    return splitString;
 };
 
 // Do not edit below this line
