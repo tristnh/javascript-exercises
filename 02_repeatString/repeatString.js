ddd34d175e9db334c32af3a9abf60565a99f86fe
Tristan <tharrisdev@gmail.com>
2022-10-11 18:45:21 -0500

    Add Body To Function To Pass Tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let val = '';
+    if (num < 0){return 'ERROR'}
+    for(let i = 0; i < num; i++ ){
+        val += string;
+    }
+    return val;
 };
 
 // Do not edit below this line
