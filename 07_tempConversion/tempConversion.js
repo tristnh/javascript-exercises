7466ea76fe4f1b70fe109fe8152ed0bcba7e7a49
Tristan <tharrisdev@gmail.com>
2022-10-12 14:23:30 -0500

    Add Body To tempConversion Function tempConversion will now pass all required tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  ctempraw = (ftemp - 32)*(5/9);
+  ctemp = Math.round(ctempraw*10)/10;
+  return ctemp;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  ftempraw = (ctemp*1.8)+32;
+  ftemp = Math.round(ftempraw*10)/10;
+  return ftemp;
 };
 
 // Do not edit below this line
