5d0080a930648163a470eb4b1d234d6d5ef32ac6
Tristan <tharrisdev@gmail.com>
2022-10-11 19:53:29 -0500

    Add Function Body To sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2<0 || !Number.isInteger(num1) || !Number.isInteger(num2)){return 'ERROR'}
 
+    if(num1>num2){
+        let temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+
+    let finalSum = num1;
+
+    for(i=0; i<num2-1; i++){
+        ++num1;
+        finalSum += num1;
+      console.log(finalSum);
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
