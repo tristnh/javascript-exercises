044be94c2ff4b6cdc6a413fad048c94196192240
Tristan <tharrisdev@gmail.com>
2022-10-18 12:59:22 -0500

    Add Function To Pass Tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let workingstring = words.toLowerCase().replace(/[^a-z]/g, '');
+    return(
+        workingstring
+        .split('')
+        .reverse()
+        .join('') == workingstring
+    );
 };
 
 // Do not edit below this line
