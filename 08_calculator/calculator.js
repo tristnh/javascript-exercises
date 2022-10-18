23cae42246e39145a85ac46a84ac442b23921333
Tristan <tharrisdev@gmail.com>
2022-10-18 12:35:47 -0500

    Add Functions To Complete Tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numlist) {
+  let init = 0;
+	return numlist.reduce((a, b) => a + b, init);
 };
 
-const multiply = function() {
-
+const multiply = function(numlist) {
+  return numlist.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let ans = 1;
+  if (n == 0 || n == 1){
+    return ans;
+  }else{
+    for(let i = n; i >= 1; i--){
+      ans = ans * i;
+    }
+    return ans;
+  }
 };
 
 // Do not edit below this line
